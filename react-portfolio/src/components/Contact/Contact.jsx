448b478dddc5077dd3782f1f19d5608d8b64ecda
mcloue25 <eoin.mcloughlin@moveahead-project.com>
2024-11-14 16:10:05 +0000

    First push of redesign of website.

---
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out if you want to talk data or anything else!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">eoin.mcloughlin1997@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/eoin-mcloughlin-3398a11a6">linkedin</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/mcloue25">github.com/mcloue25</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Eoin</h1>
+        <p className={styles.description}>
+          I'm an ML enginner & Data Scientist with ober 4 years of experience <br/>
+          Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:eoin.mcloughlin1997@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/linkedinProfilePicCircular.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
