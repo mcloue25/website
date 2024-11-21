3a9db7ef0bf561b24ccc9ae687b55ead35071e33
mcloue25 <eoin.mcloughlin@moveahead-project.com>
2024-11-21 18:09:01 +0000

    Updated project images & changed colour scheme.

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Eoin</h1>
         <p className={styles.description}>
-          I'm an ML enginner & Data Scientist with ober 4 years of experience <br/>
+          I'm an ML enginner & Data Scientist with over 4 years of experience <br/>
           Reach out if you'd like to learn more!
         </p>
         <a href="mailto:eoin.mcloughlin1997@gmail.com" className={styles.contactBtn}>
@@ -21,8 +21,8 @@ export const Hero = () => {
         alt="Hero image of me"
         className={styles.heroImg}
       />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
+      {/* <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} /> */}
     </section>
   );
 };
