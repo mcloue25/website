67ea23a2da58e53be3cfae8005729db75f46e55e
Eoin Mcloughlin <eoin.mclouglin1997@gmail.com>
2024-12-03 22:19:17 +0000

    Fixed bug in contact section & added correectly sizeed Java icon.

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="www.linkedin.com/in/eoin-mcloughlin-3398a11a6">Linkedin Profile</a>
+          <a href="https://www.linkedin.com/in/eoin-mcloughlin-3398a11a6/">Linkedin Profile</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
