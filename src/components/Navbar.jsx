aa3ba243ea8a538f515fd8d8cb8f2100b0dacead
Eoin McLoughlin <eoin.mcloughlin1997@gmail.com>
2025-09-06 14:45:29 +0100

    Added Education to Navbar>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ export const Navbar = () => {
 
   const links = [
     { href: "#about", label: "About" },
+    { href: "#education", label: "Education" },
     { href: "#experience", label: "Experience" },
     { href: "#projects", label: "Projects" },
     { href: "#contact", label: "Contact" },
